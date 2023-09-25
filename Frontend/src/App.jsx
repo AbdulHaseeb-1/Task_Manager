7ddab5b0a7f10f05ce378936d04fbcbc94b98fea
Muhammad Abdul Haseeb <144768838+AbdulHaseeb-1@users.noreply.github.com>
2023-09-25 20:27:45 +0500

    Modified

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,60 +1,42 @@
 import React from "react";
 import "./App.css";
 import TaskBox from "./Components/TaskBox";
-import {AddIcon} from "@chakra-ui/icons"
+import { AddIcon } from "@chakra-ui/icons";
+import { Button } from "@nextui-org/react";
 export default function App() {
   return (
     <>
-      <div className="p-5 md:p-10 h-[100vh]">
-        <div className="relative bg-[rgb(25,25,25)] p-3 w-auto rounded-md h-full overflow-auto">
-          <div className=" overflow-y-scroll overflow-x-auto">
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-            <TaskBox
-              task={"Meating with Mr. X on secret Project"}
-              time={"Monday, June 10 2024"}
-              status={"bg-green-500"}
-            />
-          </div>
-          <div className=" flex flex-col fixed right-5 top-5 ">
-            <button className="m-2 outline-green-500 text-white rounded-full w-10 h-10 outline-dashed grid content-center">
-              <span className="block">+</span>
-            </button>
+      <div className="p-2 md:p-10 h-[100vh]">
+     
+          <div className="relative bg-[rgb(25,25,25)] p-3 w-auto rounded-md h-full overflow-auto">
+            <h1 className="text-green-500">Task Manager</h1>
+            <div className=" overflow-y-scroll overflow-x-auto">
+              <TaskBox
+                task={"Meating with Mr. X on secret Project"}
+                time={"Monday, June 10 2024"}
+                status={"bg-red-500"}
+              />
+              <TaskBox
+                task={"Meating with Mr. X on secret Project"}
+                time={"Monday, June 10 2024"}
+                status={"bg-green-500"}
+              />
 
-            <button className="m-2 outline-red-500 text-white rounded-full w-12 h-12 outline-dashed grid content-center">
-              <span className="block"><AddIcon/></span>
-            </button>
-
-            {/* <button className="m-2 text-center outline-orange-500 text-white rounded-full w-10 h-10 outline grid content-center hover:bg-orange-500 transition">
-              <AddIcon />
-            </button> */}
-
-
-          </div>
+            
+              
+            </div>
+           
         </div>
+          <Button
+            variant="shadow"
+            isIconOnly
+            size="lg"
+            radius="full"
+            className="absolute right-5 bottom-5"
+            color="success"
+            >
+            <AddIcon />
+          </Button>
       </div>
     </>
   );
--- Frontend/src/Components/TaskBox.jsx
+++ Frontend/src/Components/TaskBox.jsx
@@ -1,9 +1,18 @@
+import { AddIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { Button } from "@nextui-org/react";
 import React from "react";
 
-export default function TaskBox({ task,time,status }) {
-
+export default function TaskBox({ task, time, status }) {
   return (
-    <div className=" relative p-2 m-3 bg-[rgb(21,21,21)] text-gray-300 h-auto shadow-lg shadow-zinc-800" >
+    <div className=" relative p-2 m-3 bg-[rgb(21,21,21)] text-gray-300 h-auto shadow-md shadow-stone-800 hover:bg-[rgb(24,24,24)]">
+      <div className="absolute right-5 top-3">
+        <Button isIconOnly radius="full" size="sm" className="m-1 text-red-500  border-transparent"  variant="ghost">
+          <DeleteIcon />
+        </Button>
+        <Button isIconOnly radius="full" size="sm" color="warning" className="m-1  border-transparent" variant="ghost">
+          <EditIcon />
+        </Button>
+      </div>
       <div className={`top-0 left-0 h-full w-0.5 ${status} absolute`}></div>
       <h6 className="font-bold ">{time}</h6>
       <p>{task}</p>
