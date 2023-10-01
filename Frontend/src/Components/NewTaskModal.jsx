26b17303616b2c49f6726948be916e55ff047412
Muhammad Abdul Haseeb <unset>
2023-10-01 22:43:15 +0500

    updated

--- Frontend/src/Components/NewTaskModal.jsx
+++ Frontend/src/Components/NewTaskModal.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input } from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+  Select,
+  SelectItem,
+} from "@nextui-org/react";
 
 function NewTask({ isOpen, onClose }) {
+const categories = [{id:1,value:""},{id:2,value:"Personal"},{id:3,value:"Work"},{id:4,value:"Wishlist"}]
+
   return (
-    <Modal isOpen={isOpen} onOpenChange={onClose} placement="top-center" backdrop="blur" className="bg-[rgb(21,21,21)] text-white" >
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onClose}
+      placement="top-center"
+      backdrop="blur"
+      className="bg-[rgb(21,21,21)] text-white"
+    >
       <ModalContent>
         {(onClose) => (
           <>
@@ -13,12 +31,24 @@ function NewTask({ isOpen, onClose }) {
                 autoFocus
                 label="Task"
                 placeholder="Enter new task here...."
-                variant="underlined"     
+                variant="underlined"
               />
 
-              <input type="datetime-local" className="bg-transparent " />
+<Select 
+        label="Select an category" 
+        variant="underlined"
+        className="max-w-xs  text-white" 
+        classNames={{listbox:"bg-[rgb(21,21,21)]",popover:"bg-[rgb(21,21,21)] text-white"}}
+      >
+        {categories.map((category) => (
+          <SelectItem key={category.id} value={category.value}>
+            {category.value}
+          </SelectItem>
+        ))}
+      </Select>
 
 
+              {/* <input type="datetime-local" className="bg-transparent " /> */}
             </ModalBody>
             <ModalFooter>
               <Button color="default" variant="flat" onPress={onClose}>
